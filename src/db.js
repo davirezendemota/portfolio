4d625fd6d7af84acdabb1b300e9e5b773bdfd8e2
Davi Rezende <rezendemota.davi@gmail.com>
2023-09-09 01:45:07 -0300

    refactor: reactjs + new profile pic

---
+++ src/db.js
@@ -0,0 +1,29 @@
+export const projectDB = [
+  {
+    tit: 'davimota.com',
+    desc: 'My website to unify personal projects, tools, carrer history, certicates and degrees.',
+    techs: ['NextJs', 'ReactJs', 'JavaScript', 'CSS/HTML'],
+    github: 'https://github.com/davirezendemota/portfolio'
+  }
+]
+
+export const skillDB = [
+  {
+    skillName: 'React',
+  },
+  {
+    skillName: 'JavaScript',
+  },
+  {
+    skillName: 'PHP',
+  },
+  {
+    skillName: 'HTML/CSS',
+  },
+  {
+    skillName: 'Git/GitHub',
+  },
+  {
+    skillName: 'Docker',
+  }
+]
